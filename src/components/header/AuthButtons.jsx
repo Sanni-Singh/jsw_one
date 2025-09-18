7660c6c85f905cf1a9c788e2cdd01eecad18d450
siva-mani-r3 <sivamaniramayanam7036@gmail.com>
2025-09-18 14:09:44 +0530

    registermodal deleted

--- src/components/header/AuthButtons.jsx
+++ src/components/header/AuthButtons.jsx
@@ -1,16 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import RegisterModal from "./RegisterModal";
 
 const AuthButtons = () => {
-  const [openRegister, setOpenRegister] = useState(false);
 
   return (
     <>
       <div className="flex sm:gap-12 gap-5">
         <button
           className="text-blue-900 sm:font-bold cursor-pointer"
-          onClick={() => setOpenRegister(true)}
+         
         >
           Register
         </button>
@@ -18,11 +16,6 @@ const AuthButtons = () => {
           Login
         </button>
       </div>
-
-      <RegisterModal
-        isOpen={openRegister}
-        onClose={() => setOpenRegister(false)}
-      />
     </>
   );
 };
